f08713581a0062aa39cc794c8de983f9c041851d
John Coleman <john.coleman@yale.edu>
2024-07-07 07:24:16 -0400

    import .vim configs

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.cmd('set runtimepath^=~/.vim runtimepath+=~/.vim/after') vim.o.packpath = vim.o.runtimepath vim.cmd('source ~/.vimrc')
 --[[
 
 =====================================================================
